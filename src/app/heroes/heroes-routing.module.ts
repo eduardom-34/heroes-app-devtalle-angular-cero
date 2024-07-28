eeec7d320118c4bd3db299743d2623d54d154f1c
eduardo 34 - pc Efrain <eduardomelchor34@gmail.com>
2024-07-28 14:02:00 -0600

    Paths de las rutas faltante

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 
 
 // localhst:4200/heroes
@@ -13,7 +16,27 @@ const routes: Routes = [
       {
         path: 'new-hero',
         component: NewPageComponent
-      }
+      },
+      {
+        path: 'search',
+        component: SearchPageComponent
+      },
+      {
+        path: 'edit/:id',
+        component: NewPageComponent
+      },
+      {
+        path: 'list',
+        component: ListPageComponent
+      },
+      {
+        path: ':id',
+        component: HeroPageComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'list'
+      },
     ]
   }
 ];
